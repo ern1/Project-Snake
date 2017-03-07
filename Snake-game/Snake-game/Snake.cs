ce3b1289d2844b0c65eed3c848974efdc17e68bd
Matte_2 <Matte_2@LENOVO-PC>
2017-03-07 15:48:48 +0100

    start

--- Snake-game/Snake-game/Snake.cs
+++ Snake-game/Snake-game/Snake.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Snake_game
 {
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
     class Snake
     {
+        
+        public int score { get; set; }
+        public int speed { get; set; }
+        public Direction direction { get; set; }
+
     }
 }
